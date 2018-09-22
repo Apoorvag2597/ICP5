51582ff7caf9f3545a5db61d26ee9f9fdf46fe2f
Apoorvag2597 <42699442+Apoorvag2597@users.noreply.github.com>
2018-09-22 15:55:46 -0500

    update

--- location/restaurant.js
+++ location/restaurant.js
@@ -6,20 +6,20 @@ app.controller('restaurantCtrl', function ($scope, $http) {
         console.log($scope.query)
 
         $http({
-            method: 'GET',
-            url: "https://api.foursquare.com/v2/venues/search" +
-                "?client_id=O10H5CV3SMKS0LEN0N1WGKJ50PKX5U30PJCQ5DEMHGTLR1RQ" +
-                "&client_secret=FCCJMSPCP1MAX4DHCAXX44W0NTALUS5L2V5IHMMXE0BFNTZB" +
+            method: 'GET', //To get the API
+            url: "https://api.foursquare.com/v2/venues/search" + //URL for API
+                "?client_id=O10H5CV3SMKS0LEN0N1WGKJ50PKX5U30PJCQ5DEMHGTLR1RQ" + //Client Id
+                "&client_secret=FCCJMSPCP1MAX4DHCAXX44W0NTALUS5L2V5IHMMXE0BFNTZB" + //Client Key
                 "&v=20160215&limit=5" +
-                "&near=" + $scope.location +
-                "&query=" + $scope.query
+                "&near=" + $scope.location + //Location entered
+                "&query=" + $scope.query //Query entered
 
         }).then(function successCallback(response) {
             //  console.log(response);
-            $scope.results = response.data.response.venues;
+            $scope.results = response.data.response.venues; //Displaying the response
             console.log($scope.results);
         }, function errorCallback(response) {
-            console.log(response);
+            console.log(response); //error response
 
         });
     }
--- translate/translate.js
+++ translate/translate.js
@@ -2,23 +2,9 @@ var app = angular.module('translateText', []);
 app.controller('translateCtrl', function ($scope,$http) {
     var text,dest;
 
-    /*$scope.getCode= function (dest) {
-        var code ="en";
-        switch (dest) {
-            case "Telugu":
-                code = "te";
-                break;
-            case "Kannada":
-                code = "kn";
-                break;
-            case "Spanish":
-                code = "es";
-                break;
-        }
-        return code;
-    }*/
+
     $scope.languages= [
-        {"lang":"Telugu","abb":"te"},
+        {"lang":"Telugu","abb":"te"}, //Languages required and their keys
         {"lang":"English","abb":"en"},
         {"lang":"Kannada","abb":"kn"},
         {"lang":"Spanish","abb":"es"},
@@ -26,22 +12,22 @@ app.controller('translateCtrl', function ($scope,$http) {
     ];
     $scope.translateText = function () {
         text=$scope.translate;
-        var source = $scope.lang;
-        var target=$scope.selectedlang;
+        var source = $scope.lang; //Source language
+        var target=$scope.selectedlang; //Output translated language
         //var target=$scope.getCode(dest);
 
         $http({
-            method: 'GET',
-            url: "https://translate.yandex.net/api/v1.5/tr.json/translate?" +
-        "key=trnsl.1.1.20151023T145251Z.bf1ca7097253ff7e."
+            method: 'GET', //For getting the API
+            url: "https://translate.yandex.net/api/v1.5/tr.json/translate?" + //To get the URL
+        "key=trnsl.1.1.20151023T145251Z.bf1ca7097253ff7e." //Key
         +
         "c0b0a88bea31ba51f72504cc0cc42cf891ed90d2&text=" + text +"&" +
         "lang="+source+"-" +target+ "&[format=plain]&[options=1]&[callback=set]"
         }).then(function successCallback(response) {
              console.log(response);
-            $scope.result = response.data.text[0]
+            $scope.result = response.data.text[0] //To display the result
         }, function errorCallback(response) {
-            console.log(response);
+            console.log(response); //Error response
 
         });
     }
